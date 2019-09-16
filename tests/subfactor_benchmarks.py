dd66238e9757ec2c6e957dc3cddcadb83a760bb3
Alexander Jadczak <jadczak@gmail.com>
2019-09-16 14:41:44 -0400

    added subfactor benchmark

---
+++ tests/subfactor_benchmarks.py
@@ -0,0 +1,19 @@
+import timeit
+import os
+import sys
+here = os.path.realpath(__file__)
+root = os.path.split(os.path.split(here)[0])[0]
+sys.path.append(root)
+
+setup = """
+from semiprimer.semiprimer import SemiPrime
+semiprime = SemiPrime("527")  # 17*31
+"""
+
+
+def main():
+    print(timeit.timeit("semiprime.sub_factor(\"7\")", setup=setup, number=10000))
+
+
+if __name__ == "__main__":
+    main()
