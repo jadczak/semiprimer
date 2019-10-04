d4b81139bb872889125697e5eb53bea6f9904e1f
Alexander Jadczak <jadczak@gmail.com>
2019-10-03 21:06:25 -0400

    removed escaping quotes in timeit function.

--- tests/subfactor_benchmarks.py
+++ tests/subfactor_benchmarks.py
@@ -1,6 +1,7 @@
 import timeit
 import os
 import sys
+
 here = os.path.realpath(__file__)
 root = os.path.split(os.path.split(here)[0])[0]
 sys.path.append(root)
@@ -12,7 +13,7 @@ semiprime = SemiPrime("527")  # 17*31
 
 
 def main():
-    print(timeit.timeit("semiprime.sub_factor(\"7\")", setup=setup, number=10000))
+    print(timeit.timeit('semiprime.sub_factor("7")', setup=setup, number=100000))
 
 
 if __name__ == "__main__":
