9241375fb6c4c2865febf36fdb412e36d3e52839
Alexander Jadczak <jadczak@gmail.com>
2019-10-06 21:53:32 -0400

    Named tuple removed.  Regular tuple faster.

--- semiprimer/semiprimer.py
+++ semiprimer/semiprimer.py
@@ -1,8 +1,6 @@
 from collections import namedtuple, deque
 from typing import Union
 
-Node = namedtuple("Node", ["a", "b", "level"])
-
 
 class SemiPrime:
     def __init__(self, semiprime: Union[str, int, float]):
@@ -23,6 +21,9 @@ class SemiPrime:
         self.truncated_semi_lookup = {
             i + 1: self.semiprime_int % 10 ** (i + 1) for i in range(len(self.semiprime_string))
         }
+        self.a = 0
+        self.b = 1
+        self.level = 2
 
     def factor(self):
         self.initialize_node_list()
@@ -30,18 +31,20 @@ class SemiPrime:
         nodes_rejected = 0
         while self.nodes:
             node = self.nodes.pop()
-            ab_product = node.a * node.b
-            if ab_product == self.semiprime_int and node.a != 1 and node.b != 1:
-                self.factors = (node.a, node.b)
+            ab_product = node[self.a] * node[self.b]
+            if ab_product == self.semiprime_int and node[self.a] != 1 and node[self.b] != 1:
+                self.factors = (node[self.a], node[self.b])
                 self.solved = True
                 print("SUCCESS")
                 print(f"Semiprime:\t{self.semiprime_string}")
-                print(f"A Factor:\t{node.a}")
-                print(f"B Factor:\t{node.b}")
+                print(f"A Factor:\t{node[self.a]}")
+                print(f"B Factor:\t{node[self.b]}")
                 print(f"Nodes Processed:\t{nodes_processed}")
                 print(f"Nodes Rejected:\t{nodes_rejected}")
                 return
-            elif ab_product > self.semiprime_int or (ab_product == self.semiprime_int and (node.a == 1 or node.b == 1)):
+            elif ab_product > self.semiprime_int or (
+                ab_product == self.semiprime_int and (node[self.a] == 1 or node[self.b] == 1)
+            ):
                 nodes_rejected += 1
                 continue
             new_nodes = self.process_node(node)
@@ -51,7 +54,7 @@ class SemiPrime:
                 print(f"Nodes Processed:  {nodes_processed}")
                 print(f"Nodes Rejected:   {nodes_rejected}")
                 print(f"Node List Length: {len(self.nodes)}")
-                print(f"Most Recent Node: a={node.a}, b={node.b}, level={node.level}\n")
+                print(f"Most Recent Node: a={node[self.a]}, b={node[self.b]}, level={node[self.level]}\n")
         if not self.solved:
             raise (ValueError(f"Could not find factors for {self.semiprime_string}.  Prime?"))
 
@@ -59,11 +62,13 @@ class SemiPrime:
         nodes = self.sub_factor(target=self.truncated_semi_lookup[1])
         self.nodes.extend(nodes)
 
-    def process_node(self, node: Node):
-        new_level = node.level + 1
+    def process_node(self, node: tuple):
+        new_level = node[self.level] + 1
         truncator = 10 ** new_level
         new_target = self.truncated_semi_lookup[new_level]
-        return self.sub_factor(target=new_target, level=new_level, a_value=node.a, b_value=node.b, truncator=truncator)
+        return self.sub_factor(
+            target=new_target, level=new_level, a_value=node[self.a], b_value=node[self.b], truncator=truncator
+        )
 
     @staticmethod
     def sub_factor(target: int, level: int = 1, a_value: int = 0, b_value: int = 0, truncator: int = 10):
@@ -78,5 +83,5 @@ class SemiPrime:
                 if current_truncated_product == target:
                     if current_a > current_b:
                         current_a, current_b = current_b, current_a
-                    ab_pairs.add(Node(a=current_a, b=current_b, level=level))
+                    ab_pairs.add((current_a, current_b, level))
         return ab_pairs
